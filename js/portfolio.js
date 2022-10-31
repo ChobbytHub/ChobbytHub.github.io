56bdd118135aef7c793621f32fba00eb766acccf
ChobbytHub <112598602+ChobbytHub@users.noreply.github.com>
2022-10-31 12:11:23 +0900

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -42,7 +42,6 @@ $('#g-nav a, footer nav a').click(function () {
   return false;//リンクの無効化
 });
 
-
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
   PositionCheck();/* 現在地を取得する関数を呼ぶ*/
@@ -59,3 +58,25 @@ $(window).resize(function() {
   //リサイズされたときの処理
   PositionCheck()
 });
+
+/*--------------------------------
+ reCAPTCHA_Sizer
+---------------------------------*/
+function reCAPTCHA_Sizer(){
+  var $w = $("#mailForm table").width(); //全体の幅
+  var $v = $w / 310; //幅に対するreCAPTCHAの比率
+  var $h = 78 * $v; //高さ
+  if($w < 500) {
+    $(".g-recaptcha").css("transform","scale(" + $v + ")");
+    $(".g-recaptcha > div").css("height",$h);
+  } else {
+    $(".g-recaptcha").css("transform","scale(1)");
+    $(".g-recaptcha > div").css("height",78);
+  }
+}
+window.onload = function () {
+  reCAPTCHA_Sizer();
+};
+window.onresize = function () {
+  reCAPTCHA_Sizer();
+};
